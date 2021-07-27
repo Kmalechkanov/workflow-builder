ffe38a97d63af8443b1224780dad36520bff529d
kmalechkanov <kaloqnmalechkanov@gmail.com>
2021-07-27 14:33:14 +0300

    Add token interceptor

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from 'src/app/pages/home/home.component';
 import { MainComponent } from './component/main.component';
 import { MainRoutingModule } from './main-routing.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from 'src/app/interceptors/token.interceptor';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,9 @@ import { MainRoutingModule } from './main-routing.module';
     MainRoutingModule,
   ],
   exports: [
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
   ],
 })
 export class MainModule { }
