4d5ff0a7def123ea16e083d255abbf97339ce9b9
Kaloyan Malechkanov <kaloqnmalechkanov@gmail.com>
2021-08-11 18:29:42 +0300

    Add array type, fix input filter

--- src/app/services/form-control.service.ts
+++ src/app/services/form-control.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Flow } from '../models/flow.model';
+import { environment as env } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root',
@@ -11,10 +12,17 @@ export class FormControlService {
     toFormGroup(flow: Flow): FormGroup {
         const group: any = {};
 
-        flow.process.variables.filter(x => x.isOutput).forEach(variable => {
-            group[variable.name] = variable.required
-                ? new FormControl('', Validators.required)
-                : new FormControl('');
+        flow.process.variables.filter(x => x.isInput).forEach(variable => {
+            let validators = [];
+
+            if (variable.required){
+                validators.push(Validators.required);
+            }
+            if (variable.schema.type == 'array') {
+                validators.push(Validators.pattern(env.regex.array.ofNumbers));
+            }
+
+            group[variable.name] = new FormControl('', Validators.compose(validators));
         });
         return new FormGroup(group);
     }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: false,
   api: 'http://localhost:4000',
+  regex: {
+    array: {
+      ofNumbers: /^\[(( )*[0-9]+( )*,)*(( )*[0-9]+( )*)?\]$/,
+      ofStrings: /^\[(( )*\"[A-z0-9 ]+\"( )*,)*(( )*\"[A-z0-9 ]+\"( )*)?\]$/,
+      ofStringsAndNumbers: /(^\[(( )*[0-9]+( )*,)*(( )*[0-9]+( )*)?\]$)|(^\[(( )*\"[A-z0-9 ]+\"( )*,)*(( )*\"[A-z0-9 ]+\"( )*)?\]$)/,
+    }
+  }
 };
