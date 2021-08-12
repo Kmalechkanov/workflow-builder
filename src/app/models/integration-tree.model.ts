7af9610aa433c3ec21b0e2b47cfe66fbb112e422
Kaloyan Malechkanov <kaloqnmalechkanov@gmail.com>
2021-08-12 13:09:12 +0300

    Change tree model

--- src/app/models/integration-tree.model.ts
+++ src/app/models/integration-tree.model.ts
@@ -3,5 +3,6 @@ import { Integration } from "./integration.model";
 export class IntegrationTree {
     root!: string;
     data?: Integration | null;
-    branches?: IntegrationTree[];
+    folders?: IntegrationTree[];
+    services?: IntegrationTree[];
 }
